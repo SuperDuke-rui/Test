aa67c7befe4f04a23572a758f4b55cda1027fd53
wr <roy_wang@trusthx.com>
2023-06-25 17:00:17 +0800

    提交developer分支

--- test.java
+++ test.java
@@ -2,5 +2,11 @@ public class Test{
 	public static void main(String args[]){
 		System.out.println("第一次添加数据");
 		System.out.println("第二次添加数据");
+		
+		initData();
+	}
+	
+	private void initData() {
+		System.out.println("我是developer分支的方法");
 	}
 }
